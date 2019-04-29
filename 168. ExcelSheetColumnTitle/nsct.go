3f9647ccf4a7ef1deedf5fdb4cefdc10e88ac506
秦启威 <qinqiwei@hotmail.com>
2019-04-29 19:21:07 +0800

    优化内存占用 168. ExcelSheetColumnTitle

--- 168. ExcelSheetColumnTitle/nsct.go
+++ 168. ExcelSheetColumnTitle/nsct.go
@@ -8,13 +8,22 @@ func main() {
 }
 
 func convertToTitle(n int) string {
-	mapI := map[int]string{
-		1: "A", 2: "B", 3: "C", 4: "D", 5: "E",
-		6: "F", 7: "G", 8: "H", 9: "I", 10: "J",
-		11: "K", 12: "L", 13: "M", 14: "N", 15: "O",
-		16: "P", 17: "Q", 18: "R", 19: "S", 20: "T",
-		21: "U", 22: "V", 23: "W", 24: "X", 25: "Y",
-		26: "Z",
+	// mapI := map[int]string{
+	// 	1: "A", 2: "B", 3: "C", 4: "D", 5: "E",
+	// 	6: "F", 7: "G", 8: "H", 9: "I", 10: "J",
+	// 	11: "K", 12: "L", 13: "M", 14: "N", 15: "O",
+	// 	16: "P", 17: "Q", 18: "R", 19: "S", 20: "T",
+	// 	21: "U", 22: "V", 23: "W", 24: "X", 25: "Y",
+	// 	26: "Z",
+	// }
+	mapI := []rune{
+		0: ' ',
+		1: 'A', 2: 'B', 3: 'C', 4: 'D', 5: 'E',
+		6: 'F', 7: 'G', 8: 'H', 9: 'I', 10: 'J',
+		11: 'K', 12: 'L', 13: 'M', 14: 'N', 15: 'O',
+		16: 'P', 17: 'Q', 18: 'R', 19: 'S', 20: 'T',
+		21: 'U', 22: 'V', 23: 'W', 24: 'X', 25: 'Y',
+		26: 'Z',
 	}
 	res := ""
 	//res := make([]rune, 0)
@@ -26,7 +35,7 @@ func convertToTitle(n int) string {
 		} else {
 			n = n / 26
 		}
-		res = mapI[i] + res
+		res = string(mapI[i]) + res
 		//fmt.Printf("i,n,s:%d,%d,%v\n", i, n, res)
 	}
 	return res
