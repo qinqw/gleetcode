f7230a507b24cc366948bacccdedc4985255260b
秦启威 <qinqiwei@hotmail.com>
2019-05-02 09:56:01 +0800

    45. Jump Game II

---
+++ 45.JumpGameII/jg2.go
@@ -0,0 +1,34 @@
+package main
+
+func main() {
+
+}
+
+/*
+   执行用时 : 16 ms, 击败了95.83% 的用户
+   内存消耗 : 10 MB, 击败了87.87% 的用户
+   Dijkstra算法的变形——将第一步第二步...第N步可以到达的节点依
+   次放入map_arrive中。或者说是对图执行广度优先遍历（图的广度
+   优先遍历可以找到某节点到所有节点最少步数，图的深度优先遍历可以
+   找到无环图的拓扑结构）
+   将第一步到达的节点放到map_arrive中
+   如果目的节点不在map_arrive中，试图从map_arrive再往前走一步
+   将可以到达的节点再次放入到map_arrive中。一直如此执行下去。
+*/
+func jump(nums []int) int {
+	steps := 0
+	start := 0
+	end := 0
+	for end < len(nums)-1 {
+		max := end
+		for i := start; i <= end; i++ {
+			if nums[i]+i > max {
+				max = nums[i] + i
+			}
+		}
+		start = end + 1
+		end = max
+		steps++
+	}
+	return steps
+}
